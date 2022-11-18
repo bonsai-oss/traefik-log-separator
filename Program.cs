25598d05e75584588b0ba521ba5a3e5822e7c351
Florian Bauer <florian@fsrv.xyz>
2022-11-18 16:07:36 +0100

    chore: initial poc

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿static IEnumerable<string> TailFrom(string file)
+{
+    using (var reader = File.OpenText(file))
+    {
+        while (true)
+        {
+            var line = reader.ReadLine();
+            if (reader.BaseStream.Length < reader.BaseStream.Position)
+                reader.BaseStream.Seek(0, SeekOrigin.Begin);
+
+            if (line != null) yield return line;
+            else Thread.Sleep(500);
+        }
+    }
+}
+
+foreach (var line in TailFrom("/tmp/test.log")) Console.WriteLine($"line read= {line}");
